5667bffe2712ae2ce5903ee298c213c76409c951
Scott Hurlow <Scottyhurlow@gmail.com>
2019-08-20 17:09:14 -0700

    adds more frontend tests

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,7 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('checks for existing config', () => {
+  console.log('done')
+})
